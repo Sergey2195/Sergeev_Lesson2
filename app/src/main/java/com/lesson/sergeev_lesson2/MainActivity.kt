4ab641fd730da963071f6328f8a06ac191173ee3
serser <Sergey21.95@gmail.com>
2023-02-15 10:08:34 +0300

    add progress bar and groups

--- app/src/main/java/com/lesson/sergeev_lesson2/MainActivity.kt
+++ app/src/main/java/com/lesson/sergeev_lesson2/MainActivity.kt
@@ -2,12 +2,30 @@ package com.lesson.sergeev_lesson2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import com.lesson.sergeev_lesson2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         setupViews()
+        setupClickListeners()
+    }
+
+    private fun setupClickListeners() {
+        binding.bottomButtonsGroup.referencedIds.forEach { view->
+            findViewById<Button>(view).setOnClickListener{setProgressState()}
+        }
+    }
+
+    private fun setProgressState(){
+        binding.progressGroup.visibility = View.GONE
+        binding.progressBar.visibility = View.VISIBLE
     }
 
     private fun setupViews() {
