38ca01d1f72f24a890a1ba3aba5473c127e7ae82
serser <Sergey21.95@gmail.com>
2023-02-18 09:25:26 +0300

    removed unnecessary files

--- app/src/main/java/com/lesson/sergeev_lesson2/Time.kt
+++
@@ -1,7 +0,0 @@
-package com.lesson.sergeev_lesson2
-
-sealed class TimeWatch(value: Int) {
-    data class Hour(val value:Int): TimeWatch(value)
-    data class Minutes(val value: Int): TimeWatch(value)
-    data class Seconds(val value: Int): TimeWatch(value)
-}
