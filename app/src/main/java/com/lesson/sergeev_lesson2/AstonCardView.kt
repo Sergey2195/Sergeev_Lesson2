7667a579aa2ff55f8b68759d59ee94b3529cf2b4
serser <Sergey21.95@gmail.com>
2023-02-19 13:45:32 +0300

    add customView into land orientation

--- app/src/main/java/com/lesson/sergeev_lesson2/AstonCardView.kt
+++ app/src/main/java/com/lesson/sergeev_lesson2/AstonCardView.kt
@@ -12,9 +12,15 @@ import com.lesson.sergeev_lesson2.databinding.AstonCardViewBinding
 class AstonCardView(
     context: Context,
     attrSet: AttributeSet?,
-    defStyleAttr: Int) : CardView(context, attrSet, defStyleAttr) {
-    constructor(context: Context): this(context, null)
-    constructor(context: Context, attrSet: AttributeSet?): this(context,attrSet, 0)
+    defStyleAttr: Int
+) : CardView(context, attrSet, defStyleAttr) {
+    constructor(context: Context) : this(context, null)
+    constructor(context: Context, attrSet: AttributeSet?) : this(
+        context,
+        attrSet,
+        R.style.AstonCardViewStyle
+    )
+
     private var binding: AstonCardViewBinding
     private var data: AstonCardData
 
@@ -23,12 +29,12 @@ class AstonCardView(
         inflater.inflate(R.layout.aston_card_view, this, true)
         binding = AstonCardViewBinding.bind(this)
         data = AstonCardData()
-        initAttrs(attrSet,defStyleAttr)
+        initAttrs(attrSet, defStyleAttr)
         initView()
     }
 
     private fun initView() {
-        with(binding){
+        with(binding) {
             astonLogo.setImageDrawable(data.imageResource)
             titleTv.text = data.titleText
             subtitleTv.text = data.subtitleText
@@ -36,32 +42,50 @@ class AstonCardView(
     }
 
     private fun initAttrs(attrSet: AttributeSet?, defStyleAttr: Int) {
-        if (attrSet == null) return
-        val typedArray = context.obtainStyledAttributes(attrSet, R.styleable.AstonCardView, defStyleAttr,0)
+        if (attrSet == null) {
+            return
+        }
+        val typedArray = context.obtainStyledAttributes(
+            attrSet,
+            R.styleable.AstonCardView,
+            defStyleAttr,
+            defStyleAttr
+        )
         data.titleText = typedArray.getString(R.styleable.AstonCardView_title_text) ?: ""
         data.subtitleText = typedArray.getString(R.styleable.AstonCardView_subtitle_text) ?: ""
         val logoRes = typedArray.getResourceId(R.styleable.AstonCardView_logo_image_res, 0)
-        data.imageResource = if (logoRes == 0){
-            null
-        }else{
-            AppCompatResources.getDrawable(context, logoRes)
-        }
+        data.imageResource =
+            if (logoRes == 0) null else AppCompatResources.getDrawable(context, logoRes)
+        val height = typedArray.getDimension(R.styleable.AstonCardView_logo_image_height, 84f)
+        val width = typedArray.getDimension(R.styleable.AstonCardView_logo_image_width, 84f)
+        binding.astonLogo.layoutParams.width = width.toInt()
+        binding.astonLogo.layoutParams.height = height.toInt()
         typedArray.recycle()
     }
 
-    fun setTitleText(text: String){
+    fun setTitleText(text: String) {
         data.titleText = text
         binding.titleTv.text = text
     }
 
-    fun setSubtitleText(text: String){
+    fun setSubtitleText(text: String) {
         data.subtitleText = text
         binding.subtitleTv.text = text
     }
 
-    fun setLogoImageResource(drawable: Drawable?){
+    fun setLogoImageResource(drawable: Drawable?) {
         data.imageResource = drawable
         binding.astonLogo.setImageDrawable(drawable)
+    }
+
+    fun setLogoSizeInDp(width: Float, height: Float) {
+        val widthDP =
+            TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, width, resources.displayMetrics)
+        val heightDP =
+            TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, height, resources.displayMetrics)
+        binding.astonLogo.layoutParams.height = heightDP.toInt()
+        binding.astonLogo.layoutParams.width = widthDP.toInt()
+        binding.astonLogo.requestLayout()
     }
 
 }
--- app/src/main/java/com/lesson/sergeev_lesson2/MainActivity.kt
+++ app/src/main/java/com/lesson/sergeev_lesson2/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.content.res.AppCompatResources
 import com.lesson.sergeev_lesson2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
