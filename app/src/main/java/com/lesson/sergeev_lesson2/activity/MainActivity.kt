d93e31f85899a405500d51fdf1cd6afe50c9b4de
serser <Sergey21.95@gmail.com>
2023-02-24 11:36:42 +0300

    refactoring after pr

--- app/src/main/java/com/lesson/sergeev_lesson2/activity/MainActivity.kt
+++ app/src/main/java/com/lesson/sergeev_lesson2/activity/MainActivity.kt
@@ -19,6 +19,7 @@ import kotlinx.coroutines.launch
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private val viewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
